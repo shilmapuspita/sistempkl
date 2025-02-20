e3b213abc6004a643815c63bc2364f8d0daa9c97
shilmapuspita <shilmachimaaa@gmail.com>
2025-02-20 13:22:00 +0700

    Menambahkan AdminModel dan update register page

---
+++ app/Models/AdminModel.php
@@ -0,0 +1,10 @@
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class AdminModel extends Model
+{
+    protected $table = 'admin';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['username', 'password'];
+}
