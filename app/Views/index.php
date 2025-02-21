14eb1148f20eb97da5a4e732881b7262315d6f82
deanpramona <deanpramona31@gmail.com>
2025-02-21 15:38:07 +0700

    perubahan routes

--- app/Views/index.php
+++ app/Views/index.php
@@ -55,7 +55,7 @@
           <li><a href="#about">About</a></li>
           <li><a href="#portfolio">Informasi</a></li>
           </li>
-          <li><a href="admin/login">Login</a></li>
+          <li><a href="/login">Login</a></li>
         </ul>
         <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
       </nav>
