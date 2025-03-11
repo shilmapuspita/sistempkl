7a8474c2c23fbe0732aa426abb742e2b082b679b
deanpramona <deanpramona31@gmail.com>
2025-03-11 10:16:15 +0700

    final pagination

--- app/Views/admin/mentor/mentor.php
+++ app/Views/admin/mentor/mentor.php
@@ -118,17 +118,6 @@
       </div>
     </div>
   </div>
-
-  <footer class="footer">
-    <div class="d-sm-flex justify-content-center justify-content-sm-between">
-      <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">
-        Copyright © 2025 <a href="https://www.bootstrapdash.com/" target="_blank">BootstrapDash</a>. All rights reserved.
-      </span>
-      <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">
-        Made with <i class="fa-solid fa-heart text-danger"></i> by Team INTI
-      </span>
-    </div>
-  </footer>
 </div>
 
 <!-- untuk search -->
