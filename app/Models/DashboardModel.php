22c38fec453ec9a60d4fbe1a0e5947fe101e442b
deanpramona <deanpramona31@gmail.com>
2025-05-02 10:48:33 +0700

    dashboard interaktif

--- app/Models/DashboardModel.php
+++ app/Models/DashboardModel.php
@@ -9,18 +9,35 @@ class DashboardModel extends Model
     protected $DBGroup = 'default';
 
     public function getTotalSiswa()
-    {
-        return $this->db->table('datasiswa')
-            ->selectCount('ID_PKL')->where('JENIS_PKL', 'PKL')
-            ->get()->getRow()->ID_PKL
-            +
-            $this->db->table('datasiswa')
-            ->selectCount('ID_PKL')->where('JENIS_PKL', 'RISET')
-            ->get()->getRow()->ID_PKL
-            +
-            $this->db->table('datapmmb')
-            ->selectCount('ID_PKL')->get()->getRow()->ID_PKL;
-    }
+{
+    $today = date('Y-m-d');
+
+    // PKL Aktif
+    $pklAktif = $this->db->table('datasiswa')
+        ->selectCount('ID_PKL')
+        ->where('JENIS_PKL', 'PKL')
+        ->where('tanggal_mulai_fix <=', $today)
+        ->where('tgl_akhir_fix >=', $today)
+        ->get()->getRow()->ID_PKL;
+
+    // RISET Aktif
+    $risetAktif = $this->db->table('datasiswa')
+        ->selectCount('ID_PKL')
+        ->where('JENIS_PKL', 'RISET')
+        ->where('tanggal_mulai_fix <=', $today)
+        ->where('tgl_akhir_fix >=', $today)
+        ->get()->getRow()->ID_PKL;
+
+    // INTERNSHIP Aktif
+    $internAktif = $this->db->table('datapmmb')
+        ->selectCount('ID_PKL')
+        ->where('TGL_AWAL <=', $today)
+        ->where('TGL_AKHIR >=', $today)
+        ->get()->getRow()->ID_PKL;
+
+    return $pklAktif + $risetAktif + $internAktif;
+}
+
 
     public function getTotalInstitusi()
     {
